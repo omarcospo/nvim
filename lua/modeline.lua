540f05fb22d80444abf4eabf40f848dce45c4bd1
Marcos Felipe <marcos.felipe@usp.br>
2024-05-25 02:04:03 -0300

    update

--- lua/modeline.lua
+++ lua/modeline.lua
@@ -1,6 +1,5 @@
 local lualine = require("lualine")
 
---- Function to get a color from a highlight group
 local function get_color(hl, part)
 	local result = vim.api.nvim_get_hl_by_name(hl, true)
 	if result[part] then
@@ -63,12 +62,10 @@ local config = {
 	},
 }
 
---- Inserts a component in lualine_c at left section
 local function ins_left(component)
 	table.insert(config.sections.lualine_c, component)
 end
 
---- Inserts a component in lualine_x at right section
 local function ins_right(component)
 	table.insert(config.sections.lualine_x, component)
 end
@@ -92,9 +89,6 @@ ins_left({
 	},
 	cond = conditions.hide_in_width,
 })
-
---- Insert mid section. You can make any number of sections in neovim :)
---- for lualine it's any number greater then 2
 ins_left({
 	function()
 		return "%="
@@ -102,7 +96,6 @@ ins_left({
 })
 
 ins_right({
-	-- Lsp server name .
 	function()
 		local msg = "No Active Lsp"
 		local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
