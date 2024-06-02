48bebff1bd2ce619282a587e06e8ae2b883d761e
Marcos Felipe <marcos.felipe@usp.br>
2024-06-02 14:14:13 -0300

    configure markdown render options

--- lua/plugins/markdownish.lua
+++ lua/plugins/markdownish.lua
@@ -4,6 +4,14 @@ return {
   ft = "markdown",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   config = function()
-    require("render-markdown").setup({})
+    require("render-markdown").setup({
+      file_types = { "markdown" },
+      headings = { "󰉫 ", "󰉬 ", "󰉭 ", "󰉮 ", "󰉯 ", "󰉰 " },
+      highlights = {
+        heading = { backgrounds = "Normal" },
+        bullet = "Normal",
+        table = { head = "Normal" },
+      },
+    })
   end,
 }
