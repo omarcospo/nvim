588020c2c968f5e6cfcc016a821a4644ecdeb1c8
Marcos Felipe <marcos.felipe@usp.br>
2024-06-05 20:05:28 -0300

    upd

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,39 +1,51 @@
 return {
+  -- {
+  --   "HoNamDuong/hybrid.nvim",
+  --   version = false,
+  --   priority = 101,
+  --   event = "UIEnter",
+  --   opts = {},
+  --   config = function()
+  --     require("hybrid").setup({
+  --       overrides = function(hl, c)
+  --         local background = "#1d1f21"
+  --         hl.TelescopeNormal = {
+  --           fg = c.fg,
+  --           bg = background,
+  --         }
+  --         hl.TelescopeBorder = {
+  --           fg = c.fg_hard,
+  --           bg = c.bg,
+  --         }
+  --         hl.TelescopeTitle = {
+  --           fg = c.fg_hard,
+  --           bg = c.bg,
+  --           bold = true,
+  --         }
+  --       end,
+  --     })
+  --     vim.cmd.colorscheme("hybrid")
+  --     vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
+  --     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+  --     if not vim.g.neovide then
+  --       vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+  --     end
+  --     vim.api.nvim_set_hl(0, "StatusLine", { link = "Comment" })
+  --     vim.api.nvim_set_hl(0, "StatusLineNC", { link = "Comment" })
+  --     vim.opt.statusline = '%{repeat("─",winwidth("."))}'
+  --   end,
+  -- },
+  -- { "savq/melange-nvim" },
   {
-    "HoNamDuong/hybrid.nvim",
+    "omarcospo/oxocarbon.nvim",
     version = false,
     priority = 101,
     event = "UIEnter",
-    opts = {},
     config = function()
-      require("hybrid").setup({
-        overrides = function(hl, c)
-          local background = "#1d1f21"
-          hl.TelescopeNormal = {
-            fg = c.fg,
-            bg = background,
-          }
-          hl.TelescopeBorder = {
-            fg = c.fg_hard,
-            bg = c.bg,
-          }
-          hl.TelescopeTitle = {
-            fg = c.fg_hard,
-            bg = c.bg,
-            bold = true,
-          }
-        end,
-      })
-      vim.cmd.colorscheme("hybrid")
-      vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+      vim.cmd.colorscheme("oxocarbon")
       if not vim.g.neovide then
         vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
       end
-      vim.api.nvim_set_hl(0, "StatusLine", { link = "Comment" })
-      vim.api.nvim_set_hl(0, "StatusLineNC", { link = "Comment" })
-      vim.opt.statusline = '%{repeat("─",winwidth("."))}'
     end,
   },
-  { "savq/melange-nvim" },
 }
