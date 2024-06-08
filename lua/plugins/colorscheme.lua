8445dc6d9524dba49eb2f872ac1f4c7f2288195d
Marcos Felipe <marcos.felipe@usp.br>
2024-06-08 09:17:55 -0300

    update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,8 +5,8 @@ return {
   event = "UIEnter",
   config = function()
     vim.cmd.colorscheme("oxocarbon")
-    -- if not vim.g.neovide then
-    --   vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
-    -- end
+    if not vim.g.neovide then
+      vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+    end
   end,
 }
