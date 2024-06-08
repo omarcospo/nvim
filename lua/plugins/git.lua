340168dfd060cf12bbda5f072321fff2b1fe14ff
Marcos Felipe <marcos.felipe@usp.br>
2024-06-07 22:45:28 -0300

    update

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,8 +2,8 @@ return {
   "NeogitOrg/neogit",
   cmd = "Neogit",
   keys = {
-    { "<leader>gg", "<cmd>Neogit cwd=%:p:h<cr>" },
-    { "<leader>gc", "<cmd>Neogit cwd=~/.dotfiles/<cr>" },
+    -- { "<leader>gg", "<cmd>Neogit cwd=%:p:h<cr>" },
+    -- { "<leader>gc", "<cmd>Neogit cwd=~/.dotfiles/<cr>" },
     { "<leader>gb", "<cmd>Telescope git_bcommits<cr>" },
     { "<leader>gl", "<cmd>Telescope git_commits<cr>" },
     { "<leader>gf", "<cmd>Telescope git_files<cr>" },
@@ -18,4 +18,12 @@ return {
       },
     })
   end,
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = { "LazyGitCurrentFile" },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>gg", ":LazyGitCurrentFile<CR>" },
+    },
+  },
 }
