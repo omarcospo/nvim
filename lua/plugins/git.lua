6a171d5028d355247b898d0839111b7f3b46e232
Marcos Felipe <marcos.felipe@usp.br>
2024-06-16 14:49:26 -0300

    update

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,14 +1,26 @@
 return {
-  "kdheepak/lazygit.nvim",
-  cmd = { "LazyGitCurrentFile" },
-  dependencies = { "nvim-lua/plenary.nvim" },
-  init = function()
-    vim.g.lazygit_floating_window_use_plenary = 0
-  end,
-  keys = {
-    { "<leader>gg", ":LazyGitCurrentFile<CR>" },
-    { "<leader>gb", "<cmd>Telescope git_bcommits<cr>" },
-    { "<leader>gl", "<cmd>Telescope git_commits<cr>" },
-    { "<leader>gf", "<cmd>Telescope git_files<cr>" },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = { "LazyGitCurrentFile" },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    init = function()
+      vim.g.lazygit_floating_window_use_plenary = 0
+    end,
+    keys = {
+      { "<leader>gg", ":LazyGitCurrentFile<CR>" },
+      { "<leader>gb", "<cmd>Telescope git_bcommits<cr>" },
+      { "<leader>gl", "<cmd>Telescope git_commits<cr>" },
+      { "<leader>gf", "<cmd>Telescope git_files<cr>" },
+    },
+  },
+  {
+    "echasnovski/mini.diff",
+    opts = {
+      view = { style = "number" },
+      mappings = {
+        apply = "gh", -- Apply hunks
+        reset = "gH", -- Reset hunks
+      },
+    },
   },
 }
