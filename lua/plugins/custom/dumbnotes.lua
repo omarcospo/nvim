6691717fe494b06eda703f9574ec5729a6fb77ed
Marcos Felipe <marcos.felipe@usp.br>
2024-06-16 18:57:38 -0300

    make it more configurable and handle errors better

--- lua/plugins/custom/dumbnotes.lua
+++ lua/plugins/custom/dumbnotes.lua
@@ -1,23 +1,37 @@
 local DumbFormat = "md"
 local DumbPath = "~/Notes"
+local new_note_key = "<C-n>"
+local delete_note_key = "<C-d>"
 
-local function open_note(prompt_bufnr)
-  local actions = require("telescope.actions")
-  local action_state = require("telescope.actions.state")
-  local selection = action_state.get_selected_entry()
-  local file_path = vim.fn.expand(DumbPath .. "/" .. selection[1])
-  actions.close(prompt_bufnr)
-  if vim.fn.filereadable(file_path) == 1 then
-    vim.cmd("edit " .. vim.fn.fnameescape(file_path))
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+local selection = action_state.get_selected_entry()
+
+local function open(input, ext)
+  local path
+  if ext == true then
+    path = vim.fn.expand(DumbPath .. "/" .. input .. "." .. DumbFormat)
+    vim.cmd("edit " .. vim.fn.fnameescape(path))
   else
-    print("Note not found: " .. file_path)
+    path = vim.fn.expand(DumbPath .. "/" .. input)
+    if vim.fn.filereadable(path) == 1 then
+      vim.cmd("edit " .. vim.fn.fnameescape(path))
+    else
+      print("Invalid note: " .. path)
+    end
   end
 end
 
+local function open_note(prompt_bufnr)
+  if selection == nil then
+    print("No valid note selected.")
+    return
+  end
+  actions.close(prompt_bufnr)
+  open(selection[1])
+end
+
 local function delete_note(prompt_bufnr)
-  local actions = require("telescope.actions")
-  local action_state = require("telescope.actions.state")
-  local selection = action_state.get_selected_entry()
   local file_path = vim.fn.expand(DumbPath .. "/" .. selection[1])
   actions.close(prompt_bufnr)
   if vim.fn.filereadable(file_path) == 1 then
@@ -38,15 +52,16 @@ local function delete_note(prompt_bufnr)
 end
 
 local function new_note(prompt_bufnr)
-  local actions = require("telescope.actions")
   actions.close(prompt_bufnr)
-  local input = vim.fn.input({ prompt = "Note title: " })
-  if input and input ~= "" then
-    local file_path = vim.fn.expand(DumbPath .. "/" .. input .. ".md")
-    print("Opening note: " .. file_path)
-    vim.cmd("edit " .. vim.fn.fnameescape(file_path))
+  local ok, input = pcall(function()
+    return vim.fn.input({ prompt = "Note title: " })
+  end)
+  if ok then
+    if input and input ~= "" then
+      open(input, true)
+    end
   else
-    print("Invalid note title.")
+    print("Error creating note: " .. input)
   end
 end
 
@@ -54,15 +69,15 @@ function DumbNotesList()
   require("telescope.builtin").find_files({
     prompt_title = "Notes",
     cwd = DumbPath,
-    find_command = { "rg", "--files", "--iglob", "*." .. DumbFormat },
+    find_command = { "fd", "-d", "1", "--extension", DumbFormat },
     attach_mappings = function(prompt_bufnr, map)
       map({ "i", "n" }, "<CR>", function()
         open_note(prompt_bufnr)
       end)
-      map({ "i", "n" }, "<c-d>", function()
+      map({ "i", "n" }, delete_note_key, function()
         delete_note(prompt_bufnr)
       end)
-      map({ "i", "n" }, "<c-n>", function()
+      map({ "i", "n" }, new_note_key, function()
         new_note(prompt_bufnr)
       end)
       return true
