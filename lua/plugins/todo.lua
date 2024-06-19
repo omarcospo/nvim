3efd23340d1009b52102c456e3ac612a16b5ad91
Marcos Felipe <marcos.felipe@usp.br>
2024-06-19 13:50:17 -0300

    add better todo keyword icons

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -2,5 +2,13 @@ return {
   "folke/todo-comments.nvim",
   event = "BufReadPost",
   dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {},
+  opts = {
+    keywords = {
+      DONE = { icon = "", color = "test" },
+      TODO = { icon = " " },
+      WARN = { icon = "", color = "warning" },
+      NOTE = { icon = "" },
+      FIX = { icon = "" },
+    },
+  },
 }
