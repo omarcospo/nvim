a21e920921643a2d19aa73c12fe22b8541c72ced
Marcos Felipe <marcos.felipe@usp.br>
2024-06-19 13:55:02 -0300

    trouble.nvim - better mappings and config

--- lua/plugins/lsp_diagnostics.lua
+++ lua/plugins/lsp_diagnostics.lua
@@ -1,13 +1,28 @@
 return {
   "folke/trouble.nvim",
-  cmd = "Trouble diagnostics toggle",
+  cmd = "Trouble",
   keys = {
     { "gt", "<CMD>Trouble diagnostics toggle filter.buf=0 focus=true<CR>" },
-    { "gs", "<CMD>Trouble lsp_references focus=true<CR>" },
+    { "gs", "<CMD>Trouble lsp_references toggle focus=true<CR>" },
+    { "<leader>ts", "<CMD>Trouble lsp_document_symbols toggle focus=false win.position=left filter.buf=0<CR>" },
   },
   opts = {
     auto_close = true,
-  }, -- for default options, refer to the configuration section for custom setup.
+    auto_preview = true,
+    auto_refresh = true,
+    auto_jump = true,
+    focus = true,
+    restore = true,
+    follow = true,
+    indent_guides = false,
+    max_items = 200,
+    multiline = false,
+    warn_no_results = false,
+    open_no_results = false,
+    icons = {
+      indent = { middle = "", last = "", top = "", ws = "" },
+    },
+  },
   init = function()
     vim.diagnostic.config({
       signs = {
